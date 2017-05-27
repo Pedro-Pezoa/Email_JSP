c8eb4d747a6d5e04d528f9eacf37f625f1dee320
MrDallOca <tdalloca@gmail.com>
2017-05-27 19:52:50 -0300

    Added support for eventual account creation

--- EmailJSP/app/controllers/account_controller.rb
+++ EmailJSP/app/controllers/account_controller.rb
@@ -1,4 +1,11 @@
 class AccountController < ApplicationController
+  
+  skip_before_action :verify_authenticity_token
+  
   def new
+    if request.method == "POST"
+      # Faz umas coisinhas e redireciona
+      redirect_back fallback_location: "/"
+    end
   end
 end
