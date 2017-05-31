7d9f0904535f33368cfcafafff7d7bf31a4ba96d
Pedro-Pezoa <pedroluizpezoa@gmail.com>
2017-05-31 11:11:18 -0300

    Estilo dos emails

--- EmailJSP/app/assets/javascripts/funcoes.js
+++ EmailJSP/app/assets/javascripts/funcoes.js
@@ -11,6 +11,19 @@ function apareceInputX(X)
     }
 }
 
+function apareceProfX(X)
+{
+    if (naoEstaEditando())
+    {
+        var divao = $('#dInfProf'+X);
+        var str = '<form method="post" class="numalinha">'+
+        '<input type="text" value="'+divao[0].innerHTML+'" name="altP" class="form-control estaEditando" required>'+
+        '<input type="number" value="'+X+'" name="altPID" class="form-control" style="visibility:hidden; position:absolute;" required>'+
+        '<button type="submit" class="btn btn-default form-control botaoAntes numalinha" style="visibility:hidden; position:absolute;"><span class="glyphicon glyphicon-search"></span></button></form>';
+        divao[0].innerHTML = str;
+    }
+}
+
 function naoEstaEditando()
 {
     return ($('.estaEditando')[0] === undefined);
